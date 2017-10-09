2039510d257cace03932e245fbd84beb0a558906
yanghualin <hualin.yang@jtsc870614.com>
2017-10-09 13:48:48 +0800

    Signed-off-by: yanghualin <hualin.yang@jtsc870614.com>

---
+++ GitBranchMerge/MainForm.Designer.cs
@@ -0,0 +1,245 @@
+﻿namespace GitBranchMerge
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_src = new System.Windows.Forms.Label();
+            this.label_target = new System.Windows.Forms.Label();
+            this.tb_src = new System.Windows.Forms.TextBox();
+            this.tb_target = new System.Windows.Forms.TextBox();
+            this.btn_submit = new System.Windows.Forms.Button();
+            this.label_project_path = new System.Windows.Forms.Label();
+            this.tb_project_path = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_folder = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tb_cloud = new System.Windows.Forms.TextBox();
+            this.label_cloud = new System.Windows.Forms.Label();
+            this.label_sysinfo = new System.Windows.Forms.Label();
+            this.tb_sysinfo = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label_src
+            // 
+            this.label_src.AutoSize = true;
+            this.label_src.Location = new System.Drawing.Point(8, 49);
+            this.label_src.Name = "label_src";
+            this.label_src.Size = new System.Drawing.Size(65, 12);
+            this.label_src.TabIndex = 0;
+            this.label_src.Text = "开发分支：";
+            this.label_src.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label_target
+            // 
+            this.label_target.AutoSize = true;
+            this.label_target.Location = new System.Drawing.Point(20, 76);
+            this.label_target.Name = "label_target";
+            this.label_target.Size = new System.Drawing.Size(53, 12);
+            this.label_target.TabIndex = 1;
+            this.label_target.Text = "主分支：";
+            // 
+            // tb_src
+            // 
+            this.tb_src.Location = new System.Drawing.Point(77, 45);
+            this.tb_src.Name = "tb_src";
+            this.tb_src.Size = new System.Drawing.Size(279, 21);
+            this.tb_src.TabIndex = 2;
+            this.tb_src.TextChanged += new System.EventHandler(this.tb_src_TextChanged);
+            // 
+            // tb_target
+            // 
+            this.tb_target.Location = new System.Drawing.Point(77, 73);
+            this.tb_target.Name = "tb_target";
+            this.tb_target.Size = new System.Drawing.Size(279, 21);
+            this.tb_target.TabIndex = 3;
+            this.tb_target.TextChanged += new System.EventHandler(this.tb_target_TextChanged);
+            // 
+            // btn_submit
+            // 
+            this.btn_submit.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_submit.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btn_submit.Location = new System.Drawing.Point(370, 49);
+            this.btn_submit.Name = "btn_submit";
+            this.btn_submit.Size = new System.Drawing.Size(85, 71);
+            this.btn_submit.TabIndex = 4;
+            this.btn_submit.Text = "执行";
+            this.btn_submit.UseVisualStyleBackColor = false;
+            this.btn_submit.Click += new System.EventHandler(this.btn_submit_Click);
+            // 
+            // label_project_path
+            // 
+            this.label_project_path.AutoSize = true;
+            this.label_project_path.Location = new System.Drawing.Point(6, 17);
+            this.label_project_path.Name = "label_project_path";
+            this.label_project_path.Size = new System.Drawing.Size(65, 12);
+            this.label_project_path.TabIndex = 5;
+            this.label_project_path.Text = "工程目录：";
+            // 
+            // tb_project_path
+            // 
+            this.tb_project_path.Cursor = System.Windows.Forms.Cursors.No;
+            this.tb_project_path.Enabled = false;
+            this.tb_project_path.Location = new System.Drawing.Point(77, 13);
+            this.tb_project_path.Name = "tb_project_path";
+            this.tb_project_path.Size = new System.Drawing.Size(279, 21);
+            this.tb_project_path.TabIndex = 6;
+            this.tb_project_path.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_folder
+            // 
+            this.btn_folder.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btn_folder.Location = new System.Drawing.Point(370, 11);
+            this.btn_folder.Name = "btn_folder";
+            this.btn_folder.Size = new System.Drawing.Size(85, 23);
+            this.btn_folder.TabIndex = 7;
+            this.btn_folder.Text = "浏览";
+            this.btn_folder.UseVisualStyleBackColor = false;
+            this.btn_folder.Click += new System.EventHandler(this.btn_folder_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.tb_cloud);
+            this.panel1.Controls.Add(this.label_cloud);
+            this.panel1.Controls.Add(this.label_sysinfo);
+            this.panel1.Controls.Add(this.tb_sysinfo);
+            this.panel1.Controls.Add(this.btn_folder);
+            this.panel1.Controls.Add(this.btn_submit);
+            this.panel1.Controls.Add(this.tb_project_path);
+            this.panel1.Controls.Add(this.label_src);
+            this.panel1.Controls.Add(this.label_project_path);
+            this.panel1.Controls.Add(this.label_target);
+            this.panel1.Controls.Add(this.tb_src);
+            this.panel1.Controls.Add(this.tb_target);
+            this.panel1.Location = new System.Drawing.Point(12, 4);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(463, 452);
+            this.panel1.TabIndex = 8;
+            // 
+            // tb_cloud
+            // 
+            this.tb_cloud.Location = new System.Drawing.Point(78, 99);
+            this.tb_cloud.Name = "tb_cloud";
+            this.tb_cloud.Size = new System.Drawing.Size(278, 21);
+            this.tb_cloud.TabIndex = 11;
+            // 
+            // label_cloud
+            // 
+            this.label_cloud.AutoSize = true;
+            this.label_cloud.Location = new System.Drawing.Point(18, 99);
+            this.label_cloud.Name = "label_cloud";
+            this.label_cloud.Size = new System.Drawing.Size(53, 12);
+            this.label_cloud.TabIndex = 10;
+            this.label_cloud.Text = "云分支：";
+            // 
+            // label_sysinfo
+            // 
+            this.label_sysinfo.AutoSize = true;
+            this.label_sysinfo.Location = new System.Drawing.Point(30, 126);
+            this.label_sysinfo.Name = "label_sysinfo";
+            this.label_sysinfo.Size = new System.Drawing.Size(41, 12);
+            this.label_sysinfo.TabIndex = 9;
+            this.label_sysinfo.Text = "信息：";
+            // 
+            // tb_sysinfo
+            // 
+            this.tb_sysinfo.BackColor = System.Drawing.SystemColors.WindowText;
+            this.tb_sysinfo.ForeColor = System.Drawing.Color.Lime;
+            this.tb_sysinfo.Location = new System.Drawing.Point(77, 126);
+            this.tb_sysinfo.Multiline = true;
+            this.tb_sysinfo.Name = "tb_sysinfo";
+            this.tb_sysinfo.ReadOnly = true;
+            this.tb_sysinfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tb_sysinfo.Size = new System.Drawing.Size(378, 321);
+            this.tb_sysinfo.TabIndex = 8;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(374, 456);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(107, 12);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Powered by yahaln";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(318, 457);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(41, 12);
+            this.linkLabel2.TabIndex = 12;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "ReadMe";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(488, 468);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "MainForm";
+            this.Text = "Git分支合并工具(win版)";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_src;
+        private System.Windows.Forms.Label label_target;
+        private System.Windows.Forms.TextBox tb_src;
+        private System.Windows.Forms.TextBox tb_target;
+        private System.Windows.Forms.Button btn_submit;
+        private System.Windows.Forms.Label label_project_path;
+        private System.Windows.Forms.TextBox tb_project_path;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+        private System.Windows.Forms.Button btn_folder;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox tb_sysinfo;
+        private System.Windows.Forms.Label label_sysinfo;
+        private System.Windows.Forms.Label label_cloud;
+        private System.Windows.Forms.TextBox tb_cloud;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+    }
+}
+
---
+++ GitBranchMerge/MainForm.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Threading;
+namespace GitBranchMerge
+{
+    public partial class MainForm : Form
+    {
+        private string filePath="";
+
+        private static string cd_project_path="cd ";
+
+        private static String cd_git_switch = "git checkout ";
+
+        private static String cd_git_pull = "git pull ";
+
+        private static String cd_git_merge = "git merge ";
+
+        private static String cd_git_status = "git status ";
+
+        private static String cd_git_push = "git push ";
+
+        private static String cd_git_cmit = "git commit ";
+
+        private delegate void FlushClient();//代理
+
+        public MainForm()
+        {
+            InitializeComponent();
+            init();
+        }
+        public void init() {
+            Win32Utility.SetCueText(tb_src,"请输入开发分支名");
+            Win32Utility.SetCueText(tb_target, "请输入主分支名");
+            Win32Utility.SetCueText(tb_cloud, "请输入云分支名");
+            this.linkLabel1.Links.Add(0, this.linkLabel1.Text.Length, @"https://yahaln.github.io/index.html");
+            this.linkLabel2.Links.Add(0, this.linkLabel2.Text.Length, @"https://yahaln.github.io/index.html"); 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_src_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_target_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 浏览文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_folder_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (string.IsNullOrEmpty(folderBrowserDialog.SelectedPath))
+                {
+                   MessageBox.Show(this, "文件夹路径不能为空", "提示");
+                    return;
+                }
+                this.filePath = folderBrowserDialog.SelectedPath;
+                this.tb_project_path.Text = this.filePath;
+                //MessageBox.Show(this, this.filePath, "提示");
+            }
+        }
+        /// <summary>
+        /// 执行按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_submit_Click(object sender, EventArgs e)
+        {
+            this.tb_sysinfo.Text = "命令执行中，请稍后。。。。。。\n";
+
+            if (string.IsNullOrEmpty(this.tb_project_path.Text))
+            {
+                MessageBox.Show(this,"git工程目录不能为空！");
+                return;
+            }
+            if (string.IsNullOrEmpty(this.tb_src.Text))
+            {
+                MessageBox.Show(this,"源分支不能为空！");
+                return;
+            }
+              if (string.IsNullOrEmpty(this.tb_target.Text))
+            {
+                MessageBox.Show(this,"目的分支不能为空！");
+                return;
+            }
+            //创建线程
+              Thread t = new Thread(threadrunnable);
+              t.IsBackground = true;
+            //执行
+              t.Start();
+
+        }
+        /// <summary>
+        /// 线程执行方法
+        /// </summary>
+        public void threadrunnable()
+        {
+            //git工程目录
+            string _projectPath = this.tb_project_path.Text.Trim();
+            //开发分支
+            string _branch_src = this.tb_src.Text.Trim();
+            //主分支
+            string _branch_target = this.tb_target.Text.Trim();
+            //云分支
+            string _branch_cloud = this.tb_cloud.Text.Trim();
+            //:的位置
+            int _index = _projectPath.IndexOf(":");
+            ///形如d:
+            string _dir = _projectPath.Substring(0, _index + 1);
+
+            try
+            {
+                //创建一个进程
+                Process p = new Process();
+                p.StartInfo.FileName = "cmd.exe";
+                p.StartInfo.UseShellExecute = false;//是否使用操作系统shell启动
+                p.StartInfo.RedirectStandardInput = true;//接受来自调用程序的输入信息
+                p.StartInfo.RedirectStandardOutput = true;//由调用程序获取输出信息
+                p.StartInfo.RedirectStandardError = true;//重定向标准错误输出
+                p.StartInfo.CreateNoWindow = true;//不显示程序窗口
+                p.Start();//启动程序
+                //向cmd窗口发送输入信息
+                //进入盘符
+                p.StandardInput.WriteLine(_dir);
+                //进入相应的目录
+                p.StandardInput.WriteLine(cd_project_path + _projectPath);
+                ////执行dir命令
+                //p.StandardInput.WriteLine("dir");
+                //切换到开发分支
+                p.StandardInput.WriteLine(cd_git_switch + _branch_src);
+                //执行拉取命令
+                p.StandardInput.WriteLine(cd_git_pull);
+                //---------------------------主start---------------------------------
+                //切换到主分支
+                p.StandardInput.WriteLine(cd_git_switch + _branch_target);
+                //执行拉取命令
+                p.StandardInput.WriteLine(cd_git_pull);
+                //执行合并命令（开发分支合并到主分支）
+                p.StandardInput.WriteLine(cd_git_merge + _branch_src);
+                //执行本地提交命令
+                p.StandardInput.WriteLine(cd_git_cmit);
+                //将本地修改缓存推到服务器上
+                p.StandardInput.WriteLine(cd_git_push);
+                //---------------------------主end---------------------------------
+
+
+                //---------------------------云start---------------------------------
+                if (string.IsNullOrEmpty(_branch_cloud))
+                {
+                    //切换到云分支
+                    p.StandardInput.WriteLine(cd_git_switch + _branch_cloud);
+                    //执行拉取命令
+                    p.StandardInput.WriteLine(cd_git_pull);
+                    //执行合并命令（主分支合并到云分支）
+                    p.StandardInput.WriteLine(cd_git_merge + _branch_target);
+                    //执行本地提交命令
+                    p.StandardInput.WriteLine(cd_git_cmit);
+                    //将本地修改缓存推到服务器上
+                    p.StandardInput.WriteLine(cd_git_push);
+                }
+                //---------------------------云end---------------------------------
+                //切换到开发分支
+                p.StandardInput.WriteLine(cd_git_switch + _branch_src);
+                //执行查看状态命令
+                p.StandardInput.WriteLine(cd_git_status);
+                //退出
+                p.StandardInput.WriteLine("exit");
+                p.StandardInput.AutoFlush = true;
+                //获取cmd窗口的输出信息
+                string output = p.StandardOutput.ReadToEnd();
+                //等待程序执行完退出进程
+                p.WaitForExit();
+                p.Close();
+                if (this.tb_sysinfo.InvokeRequired)//等待异步
+            {
+                FlushClient fc = new FlushClient(threadrunnable);
+                this.Invoke(fc);//通过代理调用刷新方法
+            }
+            else
+            {
+                this.tb_sysinfo.AppendText(output);
+                this.tb_sysinfo.AppendText("命令执行完毕！");
+            }
+                //MessageBox.Show(output);
+                //Console.WriteLine(output);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message + "\r\n跟踪;" + ex.StackTrace);
+            }
+        }
+        /// <summary>
+        /// 作者超链接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.linkLabel1.Links[this.linkLabel1.Links.IndexOf(e.Link)].Visited = true;
+            string targetUrl = e.Link.LinkData as string;
+            if (string.IsNullOrEmpty(targetUrl))
+                MessageBox.Show("没有链接地址！");
+            else
+                System.Diagnostics.Process.Start("iexplore.exe", targetUrl); 
+        }
+        /// <summary>
+        /// ReadME 超链接
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.linkLabel2.Links[this.linkLabel2.Links.IndexOf(e.Link)].Visited = true;
+            string targetUrl = e.Link.LinkData as string;
+            if (string.IsNullOrEmpty(targetUrl))
+                MessageBox.Show("没有链接地址！");
+            else
+                System.Diagnostics.Process.Start("iexplore.exe", targetUrl); 
+        }
+    }
+}
